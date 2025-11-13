ac91a83080a639d17496171bfb9018e559dfda68
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-11-13 23:39:37 +0000

    test parseEpub.ts with snapshot testing

--- test/parseEpub.spec.ts
+++ test/parseEpub.spec.ts
@@ -1,45 +1,16 @@
-import parser from '../src/epub/parseEpub'
-import _ from 'lodash'
-import * as path from 'path'
+import * as path from 'node:path'
+import { pick } from 'lodash'
+import parse from '../src/epub/parseEpub'
 
 const baseDir = process.cwd()
 const filesToBeTested = ['file-1', 'file-2', 'file-3', 'file-4', 'file-1-no-toc', 'wells']
 
-const testFile = (filename: string) => {
+for (const filename of filesToBeTested)
   describe(`parser 测试 ${filename}.epub`, () => {
-    const fileContent = parser(path.join(baseDir, `fixtures/${filename}.epub`), {
-      type: 'path',
-      expand: true,
+    test('snapshot', async () => {
+      const filePath = path.join(baseDir, `fixtures/${filename}.epub`)
+      const epub = await parse(filePath)
+      const snapshot = pick(epub, ['structure', 'info', '_spine'])
+      expect(snapshot).toMatchSnapshot()
     })
-
-    test('Result should have keys', async () => {
-      const keys = _.keys(await fileContent)
-      expect(keys.length).not.toBe(0)
-    })
-
-    test('toc', async () => {
-      const result = await fileContent
-      if (filename === 'file-1-no-toc') {
-        expect(result.structure).toBe(undefined)
-      } else {
-        expect(fileContent && typeof fileContent).toBe('object')
-      }
-    })
-
-    // it('key 分别为: flesh, nav, meta', done => {
-    //   const expectedKeys = ['flesh', 'nav', 'meta']
-
-    //   fileContent.then(result => {
-    //     const keys = _.keys(result)
-    //     keys.forEach(key => {
-    //       expect(expectedKeys.indexOf(key)).to.not.be(-1)
-    //     })
-    //     done()
-    //   })
-    // })
   })
-}
-
-filesToBeTested.forEach((filename) => {
-  testFile(filename)
-})
